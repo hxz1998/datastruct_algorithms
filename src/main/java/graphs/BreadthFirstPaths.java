11ffac624e77fcdcb117dad32567e1bd99a3812f
Monkey <1466947023@qq.com>
2019-11-08 22:18:10 +0800

    广度优先算法实现，该算法可以用来查找从起点到终点的最短路径

---
+++ src/main/java/graphs/BreadthFirstPaths.java
@@ -0,0 +1,86 @@
+/**
+ * Created by Xiaozhong on 2019/11/8.
+ * Copyright (c) 2019/11/8 Xiaozhong. All rights reserved.
+ */
+package graphs;
+
+
+import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.Queue;
+import edu.princeton.cs.algs4.Stack;
+import util.DataPathTemplate;
+
+/**
+ * 广度优先路径算法的实现
+ * <p>
+ * 在程序大致结构上和深度优先一致，但是使用了队列作为核心的数据结构对广度优先算法进行了实现
+ */
+public class BreadthFirstPaths {
+    private boolean[] marked;
+    private int[] edgeTo;
+    private final int s;
+
+    /**
+     * 根据图和起点进行广度优先路径构建
+     */
+    public BreadthFirstPaths(Graph graph, int s) {
+        this.s = s;
+        marked = new boolean[graph.V()];
+        edgeTo = new int[graph.V()];
+        bfs(graph, s);
+    }
+
+    /**
+     * 广度优先算法核心部分
+     * 使用了非递归的结构
+     */
+    public void bfs(Graph graph, int s) {
+        // 队列用于存放和当前起点s相连接的所有子节点
+        Queue<Integer> queue = new Queue<>();
+        // 将起点标记为可访问的
+        marked[s] = true;
+        // 将起点入栈
+        queue.enqueue(s);
+        while (!queue.isEmpty()) {
+            // 首先取出来最先访问到的节点，对其进行处理
+            int v = queue.dequeue();
+            // 挨个访问与取出来的节点相连接的子节点
+            for (int w : graph.adj(v)) {
+                // 如果没有标记过，那么进行访问处理
+                if (!marked[w]) {
+                    edgeTo[w] = v;
+                    marked[w] = true;
+                    // 入队
+                    queue.enqueue(w);
+                }
+            }
+        }
+    }
+
+    /**
+     * 检查是否有到达某个节点的路径
+     */
+    public boolean hashPathTo(int v) {
+        return marked[v];
+    }
+
+    /**
+     * 获取相应的路径，其基本思路和深度优先算法一致
+     */
+    public Iterable<Integer> pathTo(int v) {
+        if (!hashPathTo(v)) return null;
+        Stack<Integer> path = new Stack<>();
+        for (int i = v; i != s; i = edgeTo[i])
+            path.push(i);
+        path.push(s);
+        return path;
+    }
+
+
+    public static void main(String[] args) {
+        Graph graph = new Graph(new In(DataPathTemplate.build("tinyG.txt")));
+        DepthFirstPaths depthFirstPaths = new DepthFirstPaths(graph, 0);
+        for (Integer i : depthFirstPaths.pathTo(5))
+            System.out.print(i + " ");
+    }
+}
--- src/main/java/graphs/DepthFirstPaths.java
+++ src/main/java/graphs/DepthFirstPaths.java
@@ -1,6 +1,7 @@
 package graphs;
 
 //import edu.princeton.cs.algs4.Graph;
+
 import edu.princeton.cs.algs4.In;
 import edu.princeton.cs.algs4.Stack;
 import util.DataPathTemplate;
@@ -14,6 +15,10 @@ public class DepthFirstPaths {
     private final int s;        // 记录起点
     private int[] edgeTo;       // 与起点相连的节点的上一个节点
 
+    /**
+     * 使用图和起点构造相应的路径
+     * 构造得到的结果为与该起点相连接的所有边
+     */
     public DepthFirstPaths(Graph g, int s) {
         this.s = s;
         marked = new boolean[g.V()];
@@ -21,25 +26,42 @@ public class DepthFirstPaths {
         dfs(g, s);
     }
 
+    /**
+     * 深度优先搜索实现
+     */
     private void dfs(Graph g, int s) {
+        // 首先标记起点为可到达的
         marked[s] = true;
         for (int w : g.adj(s)) {
-            if (!marked[w]) {
+            // 对当前起点所能到达的所有子节点进行挨个访问
+            if (!marked[w]) {// 如果没有访问过，那么进行访问并标记
+                // ！！！！！！最重要的一个操作，该处记录的是子节点的父节点，也就是来时的路径
                 edgeTo[w] = s;
+                // 进行递归搜索
                 dfs(g, w);
             }
         }
     }
 
+    /**
+     * 判断是否有到达某个节点的相应路径
+     */
     public boolean hasPathTo(int v) {
         return marked[v];
     }
 
+    /**
+     * 获取到达某个节点的详细路径
+     * 使用了栈作为基本的数据结构进行存储路径
+     */
     public Iterable<Integer> pathTo(int v) {
+        // 如果没有相应的路径，那么直接返回空
         if (!hasPathTo(v)) return null;
         Stack<Integer> path = new Stack<>();
+        // 从终点，按照edgeTo中标记的父节点位置，进行路径回溯，并且压入路径栈中
         for (int i = v; i != s; i = edgeTo[i])
             path.push(i);
+        // 最后将起点也压进去
         path.push(s);
         return path;
     }
