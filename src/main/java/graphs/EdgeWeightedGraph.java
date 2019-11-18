3ad0419ac07b7ee5696592731b8541de75e7d4bb
Monkey <1466947023@qq.com>
2019-11-18 10:53:36 +0800

    加权图开始学习

---
+++ src/main/java/graphs/EdgeWeightedGraph.java
@@ -0,0 +1,25 @@
+package graphs;
+
+import edu.princeton.cs.algs4.Bag;
+
+public class EdgeWeightedGraph {
+    private final int V;
+    private final Bag<Edge> [] adj;
+
+    public EdgeWeightedGraph(int V) {
+        this.V = V;
+        adj = (Bag<Edge>[]) new Bag[V];
+        for (int i = 0; i < V; i++)
+            adj[i] = new Bag<Edge>();
+    }
+
+    public void addEdge(Edge edge) {
+        int v = edge.either(), w = edge.other(v);
+        adj[v].add(edge);
+        adj[w].add(edge);
+    }
+
+    public Iterable<Edge> adj(int v) {
+        return adj[v];
+    }
+}
