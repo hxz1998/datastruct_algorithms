ecf83261d2ca2399e464a2c355765a6f58223bf7
Monkey <1466947023@qq.com>
2019-11-19 12:26:06 +0800

    修改了一部分，但是还有问题

--- src/main/java/graphs/EdgeWeightedGraph.java
+++ src/main/java/graphs/EdgeWeightedGraph.java
@@ -1,10 +1,13 @@
 package graphs;
 
 import edu.princeton.cs.algs4.Bag;
+import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.MinPQ;
 
 public class EdgeWeightedGraph {
     private final int V;
-    private final Bag<Edge> [] adj;
+    private int E;
+    private final Bag<Edge>[] adj;
 
     public EdgeWeightedGraph(int V) {
         this.V = V;
@@ -13,13 +16,35 @@ public class EdgeWeightedGraph {
             adj[i] = new Bag<Edge>();
     }
 
+    public EdgeWeightedGraph(In in) {
+        this(in.readInt());
+        int E = in.readInt();
+        for (int i = 0; i < E; i++)
+            addEdge(new Edge(in.readInt(), in.readInt(), in.readDouble()));
+    }
+
     public void addEdge(Edge edge) {
         int v = edge.either(), w = edge.other(v);
         adj[v].add(edge);
         adj[w].add(edge);
+        E++;
     }
 
     public Iterable<Edge> adj(int v) {
         return adj[v];
+    }
+
+    public int V() {
+        return V;
+    }
+
+    public Iterable<Edge> edges() {
+        MinPQ<Edge> edges = new MinPQ<>();
+        for (int i = 0; i < adj.length; i++) {
+            for (Edge edge : adj[i]) {
+                edges.insert(edge);
+            }
+        }
+        return edges;
     }
 }
